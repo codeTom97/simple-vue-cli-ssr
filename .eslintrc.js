1317e6655c24e2efeeb8a1eb87962541a3fdd600
lhx <lhx@qq.com>
2020-09-24 11:19:25 +0800

    雨燕前端项目搭建

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/essential",
+        "eslint:recommended",
+        "@vue/typescript/recommended",
+        "@vue/prettier",
+        "@vue/prettier/@typescript-eslint"
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        // 必须为4个空格
+        indent: ["error", 4],
+        // 禁止使用 var
+        "no-var": "error",
+        // 优先使用 interface 而不是 type
+        "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+        // 生产环境 => 禁用console
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    }
 };
---
+++ .lintstagedrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "src/**/*.{js,ts,vue}": ["vue-cli-service lint", "git add"]
+};
